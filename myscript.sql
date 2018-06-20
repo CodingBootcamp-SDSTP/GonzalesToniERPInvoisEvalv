82bf74663606476ec5a741afa2b9cc52b4b892e8
toniservlet <ajaxjsonservlet.js@gmail.com>
2018-06-20 17:59:43 +0800

    4th day update database and designed login page( stylesheet.css)

--- myscript.sql
+++ myscript.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS purchaseOrders;
-DROP TABLE IF EXISTS payments;
 DROP TABLE IF EXISTS vendorInvCapture;
-DROP TABLE IF EXISTS userAccess;
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS purchaseOrders;
 DROP TABLE IF EXISTS vendors;
+DROP TABLE IF EXISTS userAccess;
 
 CREATE TABLE userAccess (
 	id VARCHAR(15) NOT NULL,
@@ -29,6 +29,10 @@ CREATE TABLE vendors (
 	PRIMARY KEY(vendorid)
 );
 
+INSERT INTO vendors (vendorId, subsidiary, vendorName, vendorAddress, taxidssn, paymentType, currency, terms, taxable) VALUES (10001, "us", "Atlassians", "San San Francisco, CA 94103", "20-3345959", "ach", "usd", "N30", "Yes");
+INSERT INTO vendors (vendorId, subsidiary, vendorName, vendorAddress, taxidssn, paymentType, currency, terms, taxable) VALUES (10002, "us", "Deltek Inc.", "509 Olive Way Seattle, WA 98101", "20-3365454", "ach", "usd", "N15", "Yes");
+INSERT INTO vendors (vendorId, subsidiary, vendorName, vendorAddress, taxidssn, paymentType, currency, terms, taxable) VALUES (10003, "us", "Dell Inc.", "Round Rock, Texas‎, USA", "20-3376464", "wire", "usd", "N45", "Yes");
+
 CREATE TABLE payments (
 	vendorId INTEGER(5) NOT NULL,
 	paymentReference INTEGER(10) NOT NULL,
@@ -36,14 +40,19 @@ CREATE TABLE payments (
 	beneficiary VARCHAR(50) NOT NULL,
 	bankName VARCHAR(50) NOT NULL,
 	accountNumber INTEGER NOT NULL,
-	iban VARCHAR(50) NOT NULL,
 	accountType VARCHAR(25) NOT NULL,
 	swiftCode VARCHAR(50) NOT NULL,
 	bankAddressNumber INTEGER NOT NULL,
 	remittanceAdd VARCHAR(50) NOT NULL,
 	PRIMARY KEY(paymentReference),
-	FOREIGN KEY(vendorId)REFERENCES vendors(vendorId)
+	FOREIGN KEY(vendorId) REFERENCES vendors(vendorId)
 );
+
+INSERT INTO payments (vendorId, paymentReference, paymentStatus, beneficiary, bankName, accountNumber, accountType, swiftCode, bankAddressNumber, remittanceAdd) VALUES (10001, 10000001, "paid", "Atlassians", "bank of america", 261232, "savings acct", "BOFAUS3N", 26123289, "North Carolina 28255");
+INSERT INTO payments (vendorId, paymentReference, paymentStatus, beneficiary, bankName, accountNumber, accountType, swiftCode, bankAddressNumber, remittanceAdd) VALUES (10001, 10000002, "paid", "Atlassians", "bank of america", 261232, "savings acct", "BOFAUS3N", 26123289, "North Carolina 28255");
+INSERT INTO payments (vendorId, paymentReference, paymentStatus, beneficiary, bankName, accountNumber, accountType, swiftCode, bankAddressNumber, remittanceAdd) VALUES (10002, 10000003, "paid", "Deltek Inc.", "bank of america", 265262, "savings acct", "BOFAUS3N", 26526288, "Ny 10038, United States Of America");
+INSERT INTO payments (vendorId, paymentReference, paymentStatus, beneficiary, bankName, accountNumber, accountType, swiftCode, bankAddressNumber, remittanceAdd) VALUES (10003, 10000004, "paid", "Dell Inc.", "bank of america", 267563, "savings acct", "BOFAUS3N", 26526288, "Ny 10038, United States Of America");
+INSERT INTO payments (vendorId, paymentReference, paymentStatus, beneficiary, bankName, accountNumber, accountType, swiftCode, bankAddressNumber, remittanceAdd) VALUES (10003, 10000005, "unpaid", "Dell Inc.", "bank of america", 267563, "savings acct", "BOFAUS3N", 26526288, "Ny 10038, United States Of America");
 
 CREATE TABLE purchaseorders (
 	vendorId INTEGER(5) NOT NULL,
@@ -57,13 +66,13 @@ CREATE TABLE purchaseorders (
 	billTo VARCHAR(50) NOT NULL,
 	shipTo VARCHAR(50) NOT NULL,
 	PRIMARY KEY(poNumber),
-	FOREIGN KEY(vendorId)REFERENCES vendors(vendorId)
+	FOREIGN KEY(vendorId) REFERENCES vendors(vendorId)
 	-- FOREIGN KEY(invoiceNumber)REFERENCES vendorInvCapture(invoiceNumber)
 );
 
 CREATE TABLE vendorInvCapture (
 	invoiceNumber VARCHAR(25) NOT NULL,
-	poNumber INTEGER(8) NOT NULL,
+	poNumber INTEGER(8),
 	vendorId INTEGER(5) 
 	NOT NULL,
 	invoiceDate VARCHAR(25) NOT NULL,
